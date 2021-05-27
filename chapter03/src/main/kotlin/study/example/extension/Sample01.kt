c9c0ef276dd651271bce58684c3365b368a0ebfc
Ddudooo <igoa.work@gmail.com>
2021-05-28 00:31:08 +0900

    3장 함수 정의와 호출
    확장 프로퍼티 추가

--- chapter03/src/main/kotlin/study/example/extension/Sample01.kt
+++ chapter03/src/main/kotlin/study/example/extension/Sample01.kt
@@ -2,9 +2,24 @@ package study.example.extension
 
 //fun String.lastChar(): Char = this.get(this.length-1)
 // 수신 객체 멤버에 this 없이 접근 가능
-fun String.lastChar(): Char = get(length-1)
+//fun String.lastChar(): Char = get(length-1)
+
+//확장 프로퍼티로 선언
+val String.lastChar: Char
+    get() = get(length-1)
+
+var StringBuilder.lastChar: Char
+    get() = get(length-1)
+    set(value: Char) {
+        this.setCharAt(length-1,value)
+    }
 
 fun main(){
     val example = "안녕하세요!"
-    println("$example is last char = ${example.lastChar()}")
+    println("$example is last char = ${example.lastChar}")
+
+    val sb = StringBuilder("Kotlin?")
+    println("StringBuilder = $sb , last char = ${sb.lastChar}")
+    sb.lastChar = '!'
+    println("StringBuilder = $sb , last char = ${sb.lastChar}")
 }
